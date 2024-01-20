f6816fb559222780c3c73e1217e5e40b3f53d8ba
Monwar Hossain <monwarhossainmunna@gmail.com>
2024-01-20 19:23:50 +0600

    challenge-1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,32 @@
+import { RootState } from "../redux/store";
+import {
+  decrement,
+  increment,
+  incrementByValue,
+} from "../redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+
 const Home = () => {
+  const count = useAppSelector((state: RootState) => state.counter.count);
+  const dispatch = useAppDispatch();
   return (
-    <div>
-      <h2>home</h2>
+    <div className="flex gap-6">
+      <button
+        onClick={() => dispatch(increment())}
+        className="btn btn-active btn-primary"
+      >
+        Increment
+      </button>
+      <p>{count}</p>
+      <button onClick={() => dispatch(decrement())} className="btn">
+        Decrement
+      </button>
+      <button
+        onClick={() => dispatch(incrementByValue(3))}
+        className="btn btn-secondary"
+      >
+        IncrementValue
+      </button>
     </div>
   );
 };
